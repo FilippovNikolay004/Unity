edc592da470181ec92009c2470451d3febe5dc0a
Filippov-Nikolay <filippovnikolay195@gmail.com>
2025-05-06 09:11:06 +0300

    fix

---
+++ Unity3D-221/Assets/Scripts/FlashLightScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FlashLightScript : MonoBehaviour
+{
+    private GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        if (player == null) {
+            Debug.Log("FlashlightScript: Player not found");
+        }
+    }
+
+
+    void Update()
+    {
+        if (player == null) return;
+    }
+}
---
+++ Unity3D-221/Assets/Scripts/LightScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    private Light[] dayLights;
+    private Light[] nightLights;
+
+    private bool isDay;
+
+    void Start()
+    {
+        dayLights = GameObject
+            .FindGameObjectsWithTag("Day")
+            .Select(g => g.GetComponent<Light>())
+            .ToArray();
+        nightLights = GameObject
+            .FindGameObjectsWithTag("Night")
+            .Select(g => g.GetComponent<Light>())
+            .ToArray();
+
+        isDay = true;
+
+        foreach (Light light in nightLights) {
+            light.intensity = 0.0f;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N)) {
+            isDay = !isDay;
+
+            if (isDay) {
+                foreach(var item in dayLights) {
+                    item.intensity = 1.0f;
+                }
+                foreach (var item in nightLights) {
+                    item.intensity = 0.0f;
+                }
+                RenderSettings.ambientIntensity = 1.0f;
+                RenderSettings.reflectionIntensity = 1.0f;
+            } else {
+                foreach (var item in dayLights) {
+                    item.intensity = 0.0f;
+                }
+                foreach (var item in nightLights) {
+                    item.intensity = 1.0f;
+                }
+                RenderSettings.ambientIntensity = 0.0f;
+                RenderSettings.reflectionIntensity = 0.0f;
+            }
+        }
+    }
+}
